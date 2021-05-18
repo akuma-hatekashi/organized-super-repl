5e01bddf6f4cb35bfb25ce94be4698d83f1d67a2
akuma hatekashi <83667192+akuma-hatekashi@users.noreply.github.com>
2021-05-18 12:55:16 -0400

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,29 @@
+import turtle
+Deku=turtle.Turtle()
+Deku.speed(0)
+for counter in range(150):
+  Deku.color("green")
+  Deku.forward(counter)
+  Deku.color("blue")
+  Deku.right(899)
+  Deku.forward(counter)
+Deku.penup()
+Deku.goto(-180,50)
+Deku.pendown()
+for counter in range(100):
+  Deku.forward(counter)
+  Deku.right(12121)
+Deku.right(180)
+Deku.penup()
+Deku.goto(200,50)
+Deku.pendown()
+for counter in range(100):
+  Deku.forward(counter)
+  Deku.right(1233)
+Deku.penup()
+Deku.goto(200,-111)
+Deku.pendown()
+for counter in range(101):
+  Deku.forward(counter)
+  Deku.right(108)
+
